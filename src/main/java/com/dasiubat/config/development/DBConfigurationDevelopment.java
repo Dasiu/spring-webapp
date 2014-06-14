3edce3f5c3aaeae17ef798bc72574795c87c5d8c
Dasiu <adam.butkiewicz@gmail.com>
2014-06-14 14:18:41 +0200

    Fixed dev profile configuration
    Before profile annotation was commented out

--- src/main/java/com/dasiubat/config/development/DBConfigurationDevelopment.java
+++ src/main/java/com/dasiubat/config/development/DBConfigurationDevelopment.java
@@ -3,12 +3,13 @@ package com.dasiubat.config.development;
 import com.dasiubat.config.SharedDBConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by Adam on 2014-05-03.
  */
 @Configuration
 @ComponentScan
-//@Profile("development")
+@Profile("development")
 public class DBConfigurationDevelopment extends SharedDBConfiguration {
 }
