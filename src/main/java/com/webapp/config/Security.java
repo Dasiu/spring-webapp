a764c316101024f8355cd7db07afa2ecf0a2b137
Dasiu <adam.butkiewicz@gmail.com>
2014-11-30 14:57:11 +0100

    Disabled CSRF protection
    * It won't be needed in internal network

--- src/main/java/com/webapp/config/Security.java
+++ src/main/java/com/webapp/config/Security.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +16,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class Security extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
