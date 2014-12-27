93ccf48b7f6b75e85027d83a2ab7c5f2d810fc9c
Dasiu <adam.butkiewicz@gmail.com>
2014-12-27 11:57:41 +0100

    Fixed SpringSecurityAuditorAware infinite loop
    * SpringSecurityAuditorAware can not call DB because otherwise it starts infinite loop

--- src/main/java/com/webapp/config/SpringSecurityAuditorAware.java
+++ src/main/java/com/webapp/config/SpringSecurityAuditorAware.java
@@ -1,26 +1,21 @@
 package com.webapp.config;
 
 import com.webapp.domain.User;
-import com.webapp.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.webapp.domain.UserDetailsUserAware;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringSecurityAuditorAware implements AuditorAware<User> {
-    @Autowired
-    private UserRepository userService;
-
     @Override
     public User getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
             return null;
         }
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return userService.findByLogin(userDetails.getUsername());
+        // can not use service/repository to query for user it causes infinite loop
+        return ((UserDetailsUserAware) authentication.getPrincipal()).getUser();
     }
 }
--- src/main/java/com/webapp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/webapp/service/UserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.webapp.service;
 
-import com.webapp.domain.UserDetailsImpl;
-import com.webapp.repository.UserRepository;
+import com.webapp.domain.UserDetailsUserAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,11 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
-    private UserRepository userService;
+    private UserService userService;
 
     @Override
     @Transactional(readOnly=true)
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-        return new UserDetailsImpl(userService.findByLogin(login));
+        return new UserDetailsUserAware(userService.findByLogin(login));
     }
 }
